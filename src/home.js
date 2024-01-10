626b59b99f45686e4a4f7902b2e5df4d6b102f16
Ogundele Caleb <ogundelecaleb13@gmail.com>
2024-01-10 19:13:30 +0100

    home changes

--- src/home.js
+++ src/home.js
@@ -103,6 +103,63 @@ const Home = () => {
           </SwiperSlide>
         </Swiper>
       </section>
+      <section className="py-[60px] md:py-[80px] gap-[30px] flex justify-between items-center bg-black ">
+        <div className="flex justify-center items-center w-full md:w-[50%]">
+          <img src="/asset/cancerRibbon.png" alt="Cancer" />
+        </div>
+        <div className=" w-full md:w-[50%] text-white">
+          <h2 className="text-[46px] font-semibold mb-3">Cancer Is Killing Our Loved Ones!</h2>
+
+          <ul className="flex flex-col gap-3">
+            <li>
+              <div className="flex justify-between itemscenter">
+                <p className="">Cervical Cancer</p>
+              </div>
+              <div className="w-full h-3 rounded-lg bg-gray-500 relative">
+                <div className="w-[90%] h-3 rounded-lg bg-[#ff3984] absolute left-0 top-0 bottom-0" />{" "}
+                <p className="absolute -top-[25px] right-[10%]">90%</p>
+              </div>
+            </li>
+            <li>
+              <div className="flex justify-between itemscenter">
+                <p>Breast Cancer</p>
+              </div>
+              <div className="w-full h-3 rounded-lg bg-gray-500 relative">
+                <div className="w-[90%] h-3 rounded-lg bg-[#ff3984] absolute left-0 top-0 bottom-0" />{" "}
+                <p className="absolute -top-[25px] right-[10%]">90%</p>
+              </div>
+            </li>
+            <li>
+              <div className="flex justify-between itemscenter">
+                <p>Lung Cancer</p>
+              </div>
+              <div className="w-full h-3 rounded-lg bg-gray-500 relative">
+                <div className="w-[75%] h-3 rounded-lg bg-[#CC24FC] absolute left-0 top-0 bottom-0" />{" "}
+                <p className="absolute -top-[25px] right-[25%]">75%</p>
+              </div>
+            </li>
+            <li>
+              <div className="flex justify-between itemscenter">
+                <p>Prostate Cancer</p>
+              </div>
+              <div className="w-full h-3 rounded-lg bg-gray-500 relative">
+                <div className="w-[95%] h-3 rounded-lg bg-red-500 absolute left-0 top-0 bottom-0" />{" "}
+                <p className="absolute -top-[25px] right-[5%]">95%</p>
+              </div>
+            </li>
+            <li>
+              <div className="flex justify-between itemscenter">
+                <p>Lymphoma</p>
+              </div>
+              <div className="w-full h-3 rounded-lg bg-gray-500 relative">
+                <div className="w-[85%] h-3 rounded-lg bg-[#4285f4] absolute left-0 top-0 bottom-0" />{" "}
+                <p className="absolute -top-[25px] right-[15%]">85%</p>
+              </div>
+            </li>
+            <li></li>
+          </ul>
+        </div>
+      </section>
     </div>
   );
 };
